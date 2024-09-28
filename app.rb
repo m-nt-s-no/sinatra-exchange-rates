0042d0baa31150850ad09a293eabee45c5cc76d0
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-09-28 19:34:38 +0000

    homepage view done

--- app.rb
+++ app.rb
@@ -4,9 +4,9 @@ require "http"
 
 get("/") do
   url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("XRATE_KEY")}"
-  @response = HTTP.get(url).to_s
-  @data = JSON.parse(@response)
-  #@currencies = @data.fetch("currencies")
+  @raw_response = HTTP.get(url)
+  @parsed_data = JSON.parse(@raw_response.to_s)
+  @currencies = @parsed_data.fetch("currencies")
 
   erb(:home)
 end
