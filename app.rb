8fde6a9e8916e3489d128b60bca3c3af217519a9
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-09-29 01:27:32 +0000

    deleted puts command in from_curr

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ end
 
 get("/:from_curr") do
   @from_curr = params.fetch("from_curr")
-  puts params
   url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("XRATE_KEY")}"
   @raw_response = HTTP.get(url)
   @parsed_data = JSON.parse(@raw_response.to_s)
