3b3546d6d2a3bc11115e47cbb5d01cc0c17287d6
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-09-29 16:02:17 +0000

    finished from_curr_to_curr route and view

--- app.rb
+++ app.rb
@@ -20,3 +20,14 @@ get("/:from_curr") do
   
   erb(:from_curr)
 end
+
+get("/:from_curr/:to_curr") do
+  @from_curr = params.fetch("from_curr")
+  @to_curr = params.fetch("to_curr")
+  url = "https://api.exchangerate.host/convert?from=#{@from_curr}&to=#{@to_curr}&amount=1&access_key=#{ENV.fetch("XRATE_KEY")}"
+  @raw_response = HTTP.get(url)
+  @parsed_data = JSON.parse(@raw_response.to_s)
+  @result = @parsed_data.fetch("result")
+
+  erb(:from_curr_to_curr)
+end
